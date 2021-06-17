6e6de65878a2a55f04984a30bf3dcacd6d422218
Rodney Santiago <GD000007711@ups.com>
2021-06-17 12:01:38 -0400

    Implementing INotifyPropertyChanged to show the bit of plumbing involved. Working up to usefulness of ReactiveExtensions.

--- ReactiveExtensionsUdemyCourse/Program.cs
+++ ReactiveExtensionsUdemyCourse/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace ReactiveExtensionsUdemyCourse
 {
@@ -7,6 +9,56 @@ namespace ReactiveExtensionsUdemyCourse
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Example0();
+
+            Console.ReadKey();
+        }
+
+        private static void Example0()
+        {
+            // Example of INotifyPropertyChanged implementation
+            // A bit of plumbing for a single property
+            var market = new Market();
+
+            market.PropertyChanged += (sender, eventArgs) =>
+            {
+                Console.WriteLine("Inside handler");
+
+                if (eventArgs.PropertyName == "Volatility")
+                {
+                    Console.WriteLine(sender.GetType().Name);
+                }
+            };
+
+            market.Volatility = 3F;
+        }
+    }
+
+    public class Market : INotifyPropertyChanged
+    {
+        private float volatility;
+
+        public float Volatility
+        {
+            get => volatility;
+            set
+            {
+                if (value.Equals(volatility))
+                {
+                    return;
+                }
+
+                volatility = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
