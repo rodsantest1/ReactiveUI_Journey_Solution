a5e81fe33ea08d559bb8d03358e6144d1f626414
Rodney Santiago <rodchar.owner@outlook.com>
2021-07-15 00:30:48 -0400

    Complete Mvp for ReactiveUI

---
+++ RxuiMvpApp/MainViewModel.cs
@@ -0,0 +1,10 @@
+﻿using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+
+namespace RxuiMvpApp
+{
+    public class MainViewModel : ReactiveObject
+    {
+        [Reactive] public double ZoomLevel { get; set; }
+    }
+}
--- RxuiMvpApp/MainWindow.xaml.cs
+++ RxuiMvpApp/MainWindow.xaml.cs
@@ -1,28 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using ReactiveUI;
+using System.Reactive.Disposables;
 
 namespace RxuiMvpApp
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : ReactiveWindow<MainViewModel>
     {
         public MainWindow()
         {
             InitializeComponent();
+
+
+            ViewModel = new MainViewModel();
+
+            this.WhenActivated(disposables =>
+            {
+
+                this.Bind(ViewModel,
+                    vm => vm.ZoomLevel,
+                    v => v.Input1.Text)
+                    .DisposeWith(disposables);
+    
+                this.OneWayBind(ViewModel,
+                    vm => vm.ZoomLevel,
+                    v => v.Label1.Text)
+                    .DisposeWith(disposables);
+            });
         }
     }
 }
