957f0de0e6057c6b3a688c9267a9e61e4b220396
Rodney Santiago <GD000007711@ups.com>
2021-07-16 14:50:52 -0400

    Wired up zoom buttons the  non ReactiveUI way

--- RxuiMvpApp/MainWindow.xaml.cs
+++ RxuiMvpApp/MainWindow.xaml.cs
@@ -15,9 +15,29 @@ namespace RxuiMvpApp
         {
             InitializeComponent();
 
+            //Note: The way I wired up zoom buttons is not the ReactiveUI way
+            //I just needed a Proof of Concept for zooming  an  ArcGIS map.
+            ZoomInButton.Click += (s, e) =>
+            {
+                MainMapView.SetViewpointScaleAsync(MainMapView.MapScale / 2);
+                //System.Diagnostics.Debug.WriteLine($"Rodney was here during in {esriMapView.MapScale}");
+            };
+
+            ZoomOutButton.Click += (s, e) =>
+            {
+                MainMapView.SetViewpointScaleAsync(MainMapView.MapScale * 2);
+                //System.Diagnostics.Debug.WriteLine($"Rodney was here during out {esriMapView.MapScale}");
+            };
+
+            /* ArcGIS map init stuff  
+             * 
+             https://developers.arcgis.com/net/  
+             https://developers.arcgis.com/net/maps-2d/tutorials/display-a-map/
+             */
             MapPoint mapCenterPoint = new MapPoint(-118.805, 34.027, SpatialReferences.Wgs84);
             MainMapView.SetViewpoint(new Viewpoint(mapCenterPoint, 100000));
 
+            /* ReactiveUI stuff */
 
             ViewModel = new MainViewModel();
 
