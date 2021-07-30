4eea675af1d40bb2ed4a3b50c6d2c35e45735b3f
Rodney Santiago <rodsan.724@outlook.com>
2021-07-30 14:50:58 -0400

    playing around with slider

--- RxuiMvpApp/MainWindow.xaml.cs
+++ RxuiMvpApp/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using ReactiveUI;
+using System;
 using System.Reactive.Disposables;
+using System.Reactive.Linq;
 
 namespace RxuiMvpApp
 {
@@ -13,6 +15,11 @@ namespace RxuiMvpApp
             InitializeComponent();
 
             ViewModel = new MainViewModel();
+
+            var slider = Observable.FromEventPattern(
+                h => SliderInput1.ValueChanged += new System.Windows.RoutedPropertyChangedEventHandler<double>(h),
+                h => SliderInput1.ValueChanged -= new System.Windows.RoutedPropertyChangedEventHandler<double>(h)
+            );
 
             this.WhenActivated(disposables =>
             {
@@ -30,6 +37,22 @@ namespace RxuiMvpApp
                     vm => vm.ZoomLevel,
                     v => v.Label1.Text)
                     .DisposeWith(disposables);
+
+                this.WhenAnyValue(x => x.ViewModel.ZoomLevel)
+                .Subscribe(x =>
+                {
+                    if (x > 0)
+                    {
+                        var displayValue = Math.Exp(0.04 * x);
+                        Label1.Text = $"{displayValue}";
+                        ViewModel.ZoomLevel = x;
+                    }
+                });
+
+                Observable.Merge(
+                    slider.Throttle(TimeSpan.FromMilliseconds(75), RxApp.MainThreadScheduler).Select(_ => SliderInput1.Value)
+                ).Subscribe(x => ViewModel.ZoomLevel = x);
+
             });
         }
     }
