af65c403bf6e1a6ff1c4985fa0c997a86818e4ce
Rodney Santiago <rodsan.724@outlook.com>
2021-07-30 17:08:41 -0400

    saving work

--- RxuiMvpApp/CustomControls/UserControl1.xaml.cs
+++ RxuiMvpApp/CustomControls/UserControl1.xaml.cs
@@ -14,9 +14,7 @@ namespace RxuiMvpApp.CustomControls
     /// </summary>
     public partial class UserControl1 : ReactiveUserControl<MapViewModel>
     {
-        const double A = -26110;
-        const double B = 26111;
-        const double C = 0.04317;
+
 
         public UserControl1()
         {
@@ -65,21 +63,37 @@ namespace RxuiMvpApp.CustomControls
                     v => v.Label1.Text)
                     .DisposeWith(disposables);
 
-                this.WhenAnyValue(x => x.ViewModel.ZoomLevel)
-                    .Subscribe(x =>
+                this.ViewModel.ZoomInteraction.RegisterHandler(interaction =>
+                {
+                    Observable.Start(() =>
                     {
-                        if (x > 0)
+
+                    }).Subscribe(x=>
+                    {
+                        var zoomLevel = this.ViewModel.ZoomLevel;
+
+                        if (zoomLevel > 0)
                         {
                             Envelope extent = (Envelope)MainMapView.GetCurrentViewpoint(ViewpointType.BoundingGeometry)?.TargetGeometry;
 
                             if (extent != null)
                             {
-                                var displayValue = GetDisplayValue(x);
-                                var updatedViewpoint = new Viewpoint(extent.GetCenter(), displayValue);
-                                MainMapView.SetViewpoint(updatedViewpoint);
-                                ViewModel.ZoomLevel = displayValue;
+                                this.ViewModel.MapPointCenter = extent.GetCenter();
+                        
+                                //ViewModel.ZoomLevel = displayValue;
                             }
                         }
+                    });
+
+                    interaction.SetOutput(this.SliderInput1.Value);    
+                });
+
+                
+
+                this.WhenAnyValue(x => x.ViewModel.ZoomLevel)
+                    .Subscribe(x =>
+                    {
+                     
                     });
 
                 //Observable.Merge(
@@ -92,18 +106,7 @@ namespace RxuiMvpApp.CustomControls
 
         }
 
-        private double GetDisplayValue(double sliderValue)
-        {
-            var displayValue = A + (B * Math.Exp(C * sliderValue));
 
-            return displayValue;
-        }
-        private double GetSliderValue(double displayValue)
-        {
-            var sliderValue = Math.Log((displayValue - A) / B) / C;
-            
-            return sliderValue;
-        }
     }
 }
 
@@ -113,10 +116,36 @@ namespace RxuiMvpApp.ViewModels
     public class MapViewModel : ReactiveObject
     {
         [Reactive] public double ZoomLevel { get; set; }
+        ReactiveCommand<Unit, Double> ZoomCommand { get; }
+        [Reactive] public double DisplayValue { get; set; }
+        [Reactive] public MapPoint MapPointCenter { get; set; }
+
+        private readonly Interaction<Unit, Double> _zoomInteraction;
+        public Interaction<Unit, Double> ZoomInteraction => this._zoomInteraction;
+
+        const double A = -26110;
+        const double B = 26111;
+        const double C = 0.04317;
 
         public MapViewModel()
         {
             SetupMap();
+
+            ZoomCommand = ReactiveCommand.CreateFromObservable<Unit, Double>(this.ZoomSlider);
+            _zoomInteraction = new Interaction<Unit, double>();
+        }
+
+        public IObservable<Double> ZoomSlider(Unit unit)
+        {
+
+            return Observable.StartAsync(async () =>
+            {
+                var currentZoomLevel = await this._zoomInteraction.Handle(unit);
+                var displayValue = GetDisplayValue(currentZoomLevel);
+                var updatedViewpoint = new Viewpoint(MapPointCenter, displayValue);
+                MainMapView.SetViewpoint(updatedViewpoint);
+                return currentZoomLevel;
+            });
         }
 
         [Reactive] public Map Map { get; set; }
@@ -125,6 +154,19 @@ namespace RxuiMvpApp.ViewModels
         {
             // Create a new map with a 'topographic vector' basemap.
             Map = new Map(BasemapStyle.ArcGISTopographic);
+        }
+
+        private double GetDisplayValue(double sliderValue)
+        {
+            var displayValue = A + (B * Math.Exp(C * sliderValue));
+
+            return displayValue;
+        }
+        private double GetSliderValue(double displayValue)
+        {
+            var sliderValue = Math.Log((displayValue - A) / B) / C;
+
+            return sliderValue;
         }
     }
 }
