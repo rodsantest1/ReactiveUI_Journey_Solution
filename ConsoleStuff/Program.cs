b182475cbbd4b07d2d33a74c681c331ab3871a54
Rodney Santiago <rodsan.724@outlook.com>
2021-07-31 23:48:58 -0400

    playing around with streaming and memory mapped files

---
+++ ConsoleStuff/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleStuff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
