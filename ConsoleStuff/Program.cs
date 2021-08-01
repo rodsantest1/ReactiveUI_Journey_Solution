bb41a172dcb81fa9755c807f44f1e2dd15fccb5d
Rodney Santiago <rodsan.724@outlook.com>
2021-08-01 00:06:49 -0400

    Testing stream vs memory mapped file

--- ConsoleStuff/Program.cs
+++ ConsoleStuff/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.IO.MemoryMappedFiles;
 
 namespace ConsoleStuff
 {
@@ -7,6 +10,51 @@ namespace ConsoleStuff
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Stopwatch sw = new Stopwatch();
+
+            sw.Start();
+
+            using (FileStream fs = new FileStream(@"c:\files\file1.txt", FileMode.Create))
+            {
+                for (int i = 0; i < 200000; i++)
+                {
+                    fs.Position = 10;
+                    fs.WriteByte(100);
+
+                    fs.Position = 5;
+                    fs.WriteByte(100);
+                    fs.Position = 15;
+                    fs.WriteByte(100);
+                    fs.Position = 3;
+                    fs.WriteByte(100);
+                }
+            }
+
+            sw.Stop();
+            Console.WriteLine($"Filestream: {sw.Elapsed}");
+            sw.Reset();
+
+            sw.Start();
+            using (MemoryMappedFile mmf =
+                MemoryMappedFile.CreateFromFile
+                (@"c:\files\file2.txt", FileMode.Create, "map1", 1000))
+            {
+                using (MemoryMappedViewAccessor accessor = mmf.CreateViewAccessor())
+                {
+                    for (int i = 0; i < 200000; i++)
+                    {
+                        accessor.Write(10, (byte)101);
+                        accessor.Write(5, (byte)100);
+                        accessor.Write(15, (byte)100);
+                        accessor.Write(3, (byte)100);
+                    }
+                }
+            }
+
+            sw.Stop();
+            Console.WriteLine($"Filestream: {sw.Elapsed}");
+            sw.Reset();
         }
     }
 }
